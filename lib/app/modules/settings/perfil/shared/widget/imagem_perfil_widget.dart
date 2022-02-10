0deaa32fa808e155bac5ebd711d33ba917951636
Rafael Munareto <rafael.munareto@icloud.com>
2022-02-10 20:28:42 -0300

    ajuste layout web

--- lib/app/modules/settings/perfil/shared/widget/imagem_perfil_widget.dart
+++ lib/app/modules/settings/perfil/shared/widget/imagem_perfil_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:mobx/mobx.dart';
 import 'package:munatasks2/app/modules/settings/perfil/models/perfil_model.dart';
 import 'package:munatasks2/app/modules/settings/perfil/shared/controller/client_store.dart';
 import 'package:munatasks2/app/shared/auth/model/user_model.dart';
