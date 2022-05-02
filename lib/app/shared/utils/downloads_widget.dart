11885c7e7bfb44424efe060733cd2cec99b8efe4
Rafael Munareto <rafael.munareto@icloud.com>
2022-05-02 16:17:14 -0300

    okok

--- lib/app/shared/utils/downloads_widget.dart
+++ lib/app/shared/utils/downloads_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+// ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
 
 class DownloadsWidget extends StatefulWidget {
