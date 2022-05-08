4fb812a20d073376e4bd9d6df53916582cb4a1de
Rafael Munareto <rafael.munareto@icloud.com>
2022-05-07 23:23:24 -0300

    ok

--- lib/app/shared/utils/splash_widget.dart
+++ lib/app/shared/utils/splash_widget.dart
@@ -22,14 +22,18 @@ class _SplashWidgetState extends State<SplashWidget> {
 
   void changeThemeStorage() async {
     await theme.get('theme').then((value) {
-      setState(() {
-        value?[0] == 'dark' ? lightMode = true : lightMode = false;
-        AppWidget.of(context)
-            ?.changeTheme(value?[0] ? ThemeMode.dark : ThemeMode.light);
-        lightMode
-            ? color = const Color(0xfff7f6f4)
-            : color = const Color(0xff042a49);
-      });
+      if (value != null) {
+        if (value.isNotEmpty) {
+          setState(() {
+            value?[0] == 'dark' ? lightMode = true : lightMode = false;
+            AppWidget.of(context)?.changeTheme(
+                value?[0] == 'dark' ? ThemeMode.dark : ThemeMode.light);
+            lightMode
+                ? color = const Color(0xfff7f6f4)
+                : color = const Color(0xff042a49);
+          });
+        }
+      }
     });
   }
 
