19ef4203d6073385ec5d48b6231611cd09d11b75
Rafael Munareto <47766522+RafaelMunareto@users.noreply.github.com>
2022-06-05 14:59:14 -0300

    ok

--- lib/app/shared/components/pop_menu_widget.dart
+++ lib/app/shared/components/pop_menu_widget.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:munatasks2/app/modules/settings/perfil/shared/model/perfil_dio_model.dart';
