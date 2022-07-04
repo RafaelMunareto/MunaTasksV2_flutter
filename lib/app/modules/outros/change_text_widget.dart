872b1944a605005c9326b73ff3cc8f313773afb0
Cadu <carlos.valsesia@caixa.gov.br>
2022-07-04 13:55:58 -0300

    alteraçoes no changelog

--- lib/app/modules/outros/change_text_widget.dart
+++ lib/app/modules/outros/change_text_widget.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart';
 
 class ChangeTextWidget extends StatelessWidget {
   final String title;
@@ -9,32 +10,62 @@ class ChangeTextWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Padding(
-        padding: const EdgeInsets.only(bottom: 8.0),
-        child: Wrap(
-          alignment: WrapAlignment.start,
-          children: [
-            const Icon(Icons.verified),
-            AutoSizeText(
-              ' $title',
-              maxLines: 1,
-              textAlign: TextAlign.left,
-              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+    return Padding(
+        padding: const EdgeInsets.only(bottom: 15.0),
+        child: Container(
+            //color: Color.fromARGB(255, 125, 194, 250),
+            width: MediaQuery.of(context).size.width * 0.55,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 172, 200, 243),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromARGB(255, 153, 153, 153),
+                  offset: const Offset(
+                    5.0,
+                    5.0,
+                  ),
+                  blurRadius: 10.0,
+                  spreadRadius: 2.0,
+                ), //BoxShadow
+                BoxShadow(
+                  color: Colors.white,
+                  offset: const Offset(0.0, 0.0),
+                  blurRadius: 0.0,
+                  spreadRadius: 0.0,
+                ), //BoxShadow
+              ],
             ),
-          ],
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.fromLTRB(8, 10, 8, 10),
-        child: AutoSizeText(
-          corpo,
-          textAlign: TextAlign.left,
-          style: const TextStyle(
-            fontSize: 16,
-          ),
-        ),
-      ),
-    ]);
+            child: Row(children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    bottom: 8.0, top: 8.0, left: 10, right: 20),
+                child: Wrap(
+                  alignment: WrapAlignment.start,
+                  verticalDirection: VerticalDirection.up,
+                  children: [
+                    const Icon(Icons.verified),
+                    AutoSizeText(
+                      ' $title',
+                      maxLines: 1,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 4, 36, 73)),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8, 20, 8, 20),
+                child: AutoSizeText(
+                  corpo,
+                  textAlign: TextAlign.left,
+                  style: const TextStyle(
+                      fontSize: 15, color: Color.fromARGB(255, 12, 22, 34)),
+                ),
+              ),
+            ])));
   }
 }
--- lib/app/modules/outros/changelog_page.dart
+++ lib/app/modules/outros/changelog_page.dart
@@ -26,24 +26,24 @@ class ChangelogPageState extends State<ChangelogPage> {
             child: Column(children: const [
               ChangeTextWidget(
                 title: 'Versão 2.2.0',
-                corpo: ' - Alteração do layout do thead do create. \n\n'
-                    ' - Inclusão do atalho no create quando falta algum item obrigatório.\n\n'
-                    ' - Fechamento do search quando altera alguma tarefa. \n\n'
-                    ' - Inclusão do loading nas alterações de tarefas. \n\n'
-                    ' - Ajuste do bug do botão novo da subtarefa que não apagava o texto.\n\n'
-                    ' - Ajuste do Web Socket e do delete. \n\n'
-                    ' - Inclusão do loading nas alterações de tarefas. \n\n'
-                    ' - Aumento do tamanho do dialog para 100%.\n\n'
-                    ' - Fechamento do search com update e save.\n\n'
-                    ' - Bug da formatação do privacy.\n\n'
-                    ' - Ajuste do bug de envio de emails com email suporte@munatask.com. \n\n',
+                corpo: ' ✓ Alteração do layout do thead do create. \n\n'
+                    ' ✓ Inclusão do atalho no create quando falta algum item obrigatório.\n\n'
+                    ' ✓ Fechamento do search quando altera alguma tarefa. \n\n'
+                    ' ✓ Inclusão do loading nas alterações de tarefas. \n\n'
+                    ' ✓ Ajuste do bug do botão novo da subtarefa que não apagava o texto.\n\n'
+                    ' ✓ Ajuste do Web Socket e do delete. \n\n'
+                    ' ✓ Inclusão do loading nas alterações de tarefas. \n\n'
+                    ' ✓ Aumento do tamanho do dialog para 100%.\n\n'
+                    ' ✓ Fechamento do search com update e save.\n\n'
+                    ' ✓ Bug da formatação do privacy.\n\n'
+                    ' ✓ Ajuste do bug de envio de emails com email suporte@munatask.com. ',
               ),
               ChangeTextWidget(
                 title: 'Versão 2.1.8',
                 corpo:
-                    ' - Criação do changelog ao clicar no ícone da versão. \n\n'
-                    ' - Ajuste do botão incluir subtarefas nos aparelhos mais antigos.\n\n'
-                    ' - Ajuste do bug do popup de atualização da versão mobile. \n\n',
+                    ' ✓ Criação do changelog ao clicar no ícone da versão. \n\n'
+                    ' ✓ Ajuste do botão incluir subtarefas nos aparelhos mais antigos.\n\n'
+                    ' ✓ Ajuste do bug do popup de atualização da versão mobile. ',
               ),
             ]),
           ),
